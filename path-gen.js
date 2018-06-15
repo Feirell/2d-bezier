0bcb52f01104bf83af86dcc38b2f11898e881326
Feirell <Feirell@users.noreply.github.com>
2018-06-16 00:11:15 +0200

    nice visualization

--- path-gen.js
+++ path-gen.js
@@ -1,131 +1,68 @@
 (function () {
     const svgContainer = document.getElementById('svg-container');
     const tSlider = document.getElementById('t-slider');
-    const namespace = 'http://www.w3.org/2000/svg';
-
-    const createSVGElement = document.createElementNS.bind(document, namespace);
-
-    function createCircleAt(x, y, className) {
-        const circle = createSVGElement('circle');
-        circle.cx.baseVal.value = x;
-        circle.cy.baseVal.value = y;
-        circle.r.baseVal.value = 2.5;
-
-        circle.className.baseVal = className;
-
-        // circle.style.fill = "rgb(200,20,20)";
-
-        return circle;
-    }
-
-    function createPolyline(className, ...points) {
-        if (points[0] instanceof Array)
-            points = points[0];
-
-        const polyline = createSVGElement('polyline');
-        console.log(polyline);
-        for (point of points) {
-            const svgPoint = svgContainer.createSVGPoint();
-            svgPoint.x = point.x;
-            svgPoint.y = point.y;
-
-            polyline.points.appendItem(svgPoint);
-        }
-
-        polyline.style.fill = "none";
-        polyline.style.stroke = "black";
-
-        return polyline;
-    }
-
-    function createLine(className, a, b) {
-        const line = createSVGElement('line');
-
-        line.x1.baseVal.value = a.x;
-        line.y1.baseVal.value = a.y;
-
-        line.x2.baseVal.value = b.x;
-        line.y2.baseVal.value = b.y;
-
-        line.className.baseVal = className;
-        line.style.stroke = 'black';
-
-        return line;
-    }
-
-    // const c1 = createCircleAt(0, 0);
-    // const c2 = createCircleAt(300, 200);
 
 
-    function drawBezier(className, step, holdingPoints) {
+    // function drawBezier(className, step, holdingPoints) {
+    //     const bezier = new(Function.prototype.bind.apply(Bezier, [undefined].concat(holdingPoints)));
+
+    //     for (holdingPoint of holdingPoints) {
+    //         svgContainer.appendChild(createCircleAt(holdingPoint.x, holdingPoint.y, 'holding-point ' + className));
+    //     }
+
+    //     svgContainer.appendChild(createPolyline('holding', holdingPoints));
+
+    //     for (let t = 0; t <= 1; t += step) {
+    //         const point = bezier.at(t);
+    //         svgContainer.appendChild(createCircleAt(point.x, point.y, 'intermidiate ' + className));
+    //     }
+    // }
+
+    function interactiveBezier(simpleSVG, className, holdingPoints) {
+        console.log('simpleSVG', simpleSVG);
         const bezier = new(Function.prototype.bind.apply(Bezier, [undefined].concat(holdingPoints)));
 
-        for (holdingPoint of holdingPoints) {
-            svgContainer.appendChild(createCircleAt(holdingPoint.x, holdingPoint.y, 'holding-point ' + className));
-        }
-
-        svgContainer.appendChild(createPolyline('holding', holdingPoints));
-
-        for (let t = 0; t <= 1; t += step) {
-            const point = bezier.at(t);
-            svgContainer.appendChild(createCircleAt(point.x, point.y, 'intermidiate ' + className));
-        }
-    }
-
-    function interactiveBezier(className, step, holdingPoints) {
-        const bezier = new(Function.prototype.bind.apply(Bezier, [undefined].concat(holdingPoints)));
-
-        for (holdingPoint of holdingPoints) {
-            svgContainer.appendChild(createCircleAt(holdingPoint.x, holdingPoint.y, 'holding-point ' + className));
-        }
-
-        svgContainer.appendChild(createPolyline('holding', holdingPoints));
-
-        let circle;
-        let dots = [];
+        let interDots = [];
         let interDotLines = [];
 
-        interDotLines
+        const c = {
+            x: 0,
+            y: 0
+        };
+
+        const dotsAmount = util.sum(holdingPoints.length);
+        const linesAmount = util.sum(holdingPoints.length - 1);
+
+
+        for (let i = 0; i < linesAmount; i++) {
+            const level = util.level(bezier.grade - 1, i);
+            const itemClassString = 'line ' + className + ' ' + ((level == 0 || i == dotsAmount - 1) ? (level == 0 ? 'holding' : 'final') : 'intermidiate intermidiate-' + level);
+            interDotLines[i] = simpleSVG.createLine(itemClassString, c, c);
+        }
+
+        for (let i = 0; i < dotsAmount; i++) {
+            const level = util.level(bezier.grade, i);
+            const itemClassString = 'point ' + className + ' ' + ((level == 0 || i == dotsAmount - 1) ? (level == 0 ? 'holding' : 'final') : 'intermidiate intermidiate-' + level) + ' pointname-' + String.fromCharCode(65 + i);
+            interDots[i] = simpleSVG.createCircleAt(0, 0, itemClassString);
+        }
 
         function setPointAt(t) {
             let points = bezier.atWithIntermidiate(t);
 
-            if (!circle) {
-                circle = createCircleAt(points.point.x, points.point.y, 'intermidiate ' + className);
-                svgContainer.appendChild(circle);
-            } else {
-                circle.cx.baseVal.value = points.point.x;
-                circle.cy.baseVal.value = points.point.y;
-            }
+            let interDotLineCounter = 0;
+            for (let i = 0; i < points.length; i++) {
 
+                interDots[i].cx.baseVal.value = points[i].x;
+                interDots[i].cy.baseVal.value = points[i].y;
 
-            for (let i = 0; i < intermediatesPoints.length; i++) {
-                let intermidiateLevel = calcLevel(i, bezier.grade);
+                if (util.level(bezier.grade, i) == util.level(bezier.grade, i + 1)) {
+                    interDotLines[interDotLineCounter].x1.baseVal.value = points[i].x;
+                    interDotLines[interDotLineCounter].y1.baseVal.value = points[i].y;
+                    interDotLines[interDotLineCounter].x2.baseVal.value = points[i + 1].x;
+                    interDotLines[interDotLineCounter].y2.baseVal.value = points[i + 1].y;
 
-                if (!dots[i]) {
-                    dots[i] = createCircleAt(intermediatesPoints[i].x, intermediatesPoints[i].y, 'intermidiate-' + intermidiateLevel + ' ' + className);
-                    dots[i].id = 'intermidiate-' + i;
-                    svgContainer.appendChild(dots[i]);
-                } else {
-                    dots[i].cx.baseVal.value = intermediatesPoints[i].x;
-                    dots[i].cy.baseVal.value = intermediatesPoints[i].y;
+                    interDotLineCounter++;
                 }
-
-
-                if ((intermidiateLevel == calcLevel(i + 1, bezier.grade)) && !isNaN(calcLevel(i + 1, bezier.grade))) {
-                    // console.log('using', i, i + 1)
-                    if (!interDotLines[i]) {
-                        interDotLines[i] = createLine('intermidiate-line-' + intermidiateLevel + ' ' + className, intermediatesPoints[i], intermediatesPoints[i + 1]);
-                        svgContainer.appendChild(interDotLines[i]);
-                    } else {
-                        interDotLines[i].x1.baseVal.value = intermediatesPoints[i].x;
-                        interDotLines[i].y1.baseVal.value = intermediatesPoints[i].y;
-                        interDotLines[i].x2.baseVal.value = intermediatesPoints[i + 1].x;
-                        interDotLines[i].y2.baseVal.value = intermediatesPoints[i + 1].y;
-                    }
-                }
-                // else console.log(i, 'and', i + 1, 'do nate have the same level of ', calcLevel(i, bezier.grade), 'and', calcLevel(i + 1, bezier.grade))
-
             }
         }
 
@@ -135,6 +72,8 @@
             setPointAt: setPointAt
         }
     }
+
+
     /*
         drawBezier('bez-1', 0.05, [{
                 x: 30,
@@ -153,7 +92,9 @@
             }
         ])
     */
-    const interactive = interactiveBezier('bez-1', 0.05, [{
+
+    const interactives = []
+    interactives[interactives.length] = interactiveBezier(new SimpleSVG(document.getElementById('svg-container-a')), 'bez-1', [{
             x: 30,
             y: 30
         },
@@ -170,10 +111,36 @@
         }
     ]);
 
+    interactives[interactives.length] = interactiveBezier(new SimpleSVG(document.getElementById('svg-container-b')), 'bez-2', [{
+            x: 20,
+            y: 80
+        },
+        {
+            x: 40,
+            y: 40
+        }, {
+            x: 60,
+            y: 20
+        },
+        {
+            x: 100,
+            y: 20
+        },
+        {
+            x: 120,
+            y: 40
+        },
+        {
+            x: 140,
+            y: 80
+        }
+    ]);
+
     tSlider.addEventListener('input', function (ev) {
         const t = parseFloat(ev.target.value);
-        interactive.setPointAt(t);
-
+        // interactive.setPointAt(t);
+        for (let int of interactives)
+            int.setPointAt(t);
     })
 
 })()
