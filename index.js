12cf4af5dba00d4a45859f2347b40ed0c2ec0eb1
Florian Völker <Neirolf2030@web.de>
2018-10-03 16:05:18 +0200

    splitted produced bezier in own files

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export * from './lib/bezier.js';
-export * from './lib/produced-bezier.js'
+export * from './lib/produced-general-bezier.js'
+export * from './lib/produced-specific-bezier.js'
 export * from './lib/cubic-bezier.js';
