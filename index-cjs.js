fd67ca5bd03f30d9ba30d4f326e22d1bbba18549
Florian Völker <Neirolf2030@web.de>
2018-10-03 18:03:09 +0200

    updated index-cjs.js for 1.0.0

--- index-cjs.js
+++ index-cjs.js
@@ -3,8 +3,6 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 
 const factorial = (function () {
-    // taken from: https://stackoverflow.com/a/3959275
-
     const factorialCache = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600, 6227020800, 87178291200, 1307674368000, 20922789888000, 355687428096000, 6402373705728000];
 
     function factorial(n) {
