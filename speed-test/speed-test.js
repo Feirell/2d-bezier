26854a5e60bde3d8e400c6dc4d2fc661e3a19517
Florian Völker <Neirolf2030@web.de>
2018-10-04 18:25:36 +0200

    splitted code for result formatting and speed-testing

--- speed-test/speed-test.js
+++ speed-test/speed-test.js
@@ -5,6 +5,10 @@ import {
     CubicBezier
 } from '../index.js';
 
+import {
+    stringifySuite
+} from './result-formatter.js';
+
 const pa = {
     x: 0,
     y: 0
@@ -72,71 +76,6 @@ for (let variant of variantsToTest) {
     const calcedPoint = variant.instance.at(0.3);
     if (Math.abs(0.2412 - calcedPoint.x) > 0.000001 || Math.abs(0.342 - calcedPoint.y) > 0.000001)
         throw new Error('variant "' + variant.name + '" did not returned an accaptable result for the test calculation, correct would be x: 0.2412 and y: 0.342 but returned x: ' + calcedPoint.x + ' and y: ' + calcedPoint.y);
-}
-
-function createTableLikeOutput(columnNames, rows) {
-    const maxSizeColumns = columnNames.map(n => n.length);
-    const columns = columnNames.length;
-
-    for (let r = 0; r < rows.length; r++) {
-        const row = rows[r];
-
-        if (columns < row.length)
-            throw new Error("row " + r + " has more columns than defined in columnNames");
-
-        for (let c = 0; c < columns; c++)
-            if (maxSizeColumns[c] < row[c].length)
-                maxSizeColumns[c] = row[c].length;
-    }
-
-    let str = "";
-    for (let c = 0; c < columns; c++) {
-        str += columnNames[c].padStart(maxSizeColumns[c], ' ');
-
-        if (c != columns - 1)
-            str += " ";
-    }
-
-    str += '\n';
-
-    for (let r = 0; r < rows.length; r++) {
-        const row = rows[r];
-        for (let c = 0; c < columns; c++) {
-            str += row[c].padStart(maxSizeColumns[c], ' ');
-            if (c != columns - 1)
-                str += " ";
-        }
-
-        if (r != rows.length - 1)
-            str += '\n';
-    }
-
-    return str;
-}
-
-const integerFormatter = Intl.NumberFormat('en-US', {
-    maximumFractionDigits: 0
-});
-const floatFormatter = Intl.NumberFormat('en-US', {
-    maximumFractionDigits: 2
-});
-
-function formatNumber(integer, number) {
-    if (number == 0)
-        return '';
-
-    return (integer ? integerFormatter : floatFormatter).format(number);
-}
-
-function stringifySuite(suite) {
-    return createTableLikeOutput(['name', 'ops/sec', 'variance', 'samples'], suite.map(
-        benchmark => [
-            '' + benchmark.name,
-            '' + formatNumber(true, benchmark.hz),
-            '' + formatNumber(false, benchmark.stats.rme),
-            '' + formatNumber(true, benchmark.stats.sample.length)
-        ]
-    ));
 }
 
 function printStatus() {
